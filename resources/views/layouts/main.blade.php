d916dd43c86a9ba47282fad88b35173303aa3d02
lyustiz <you@example.com>
2021-01-15 16:14:14 -0400

    update font

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,7 @@
 
      <!-- Material Icons -->
     <link rel="stylesheet" href="/assets/googlefonts/css/css.css">
+    <link href="http://fonts.cdnfonts.com/css/komika-axis" rel="stylesheet">
 	
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
